c7baff9a0e31384e71edd044fe4fbe101b43799d
Madison Owens <madison.owens1221@gmail.com>
2017-06-26 16:30:01 -0500

    finished part 2 (setting up a one to many relationship) without bonus

--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -1,8 +1,11 @@
 package org.launchcode.models;
 
+import org.apache.catalina.startup.Catalina;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -24,7 +27,8 @@ public class Cheese {
     @Size(min=1, message = "Description must not be empty")
     private String description;
 
-    private CheeseType type;
+    @ManyToOne
+    private Category category;
 
     public Cheese(String name, String description) {
         this.name = name;
@@ -40,7 +44,6 @@ public class Cheese {
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
@@ -48,16 +51,15 @@ public class Cheese {
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
 
-    public CheeseType getType() {
-        return type;
+    public Category getCategory() {
+        return category;
+    }
+    public void setCategory(Category category) {
+        this.category = category;
     }
 
-    public void setType(CheeseType type) {
-        this.type = type;
-    }
 }
